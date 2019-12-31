85989217cc6f05f7877ea64e85ef5e809eeecfa5
Captaintesla1998 <53061372+Captaintesla1998@users.noreply.github.com>
2020-01-01 04:13:20 +0530

    Create friend.cpp

---
+++ friend.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Avengers{
+  public:
+    Avengers(){
+      IronMan=0;
+    }
+  private:
+    int IronMan;
+friend void AvengersEnemy(Avengers obj);
+};
+
+void AvengersEnemy(Avengers obj){
+  Avengers Endgame
+  Endgame.IronMan = 3000;
+  cout << Endgame.IronMan << endl;
+}
+
+int main(void){
+  Avengers Cap;
+  AvengersEnemy(Cap);
+}
